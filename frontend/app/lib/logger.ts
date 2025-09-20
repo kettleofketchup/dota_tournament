268db2a1cd779b1f641c88b1fb950df567a1d665
kettleofketchup@gmail.com <kettleofketchup@gmail.com>
2025-09-20 19:50:00 -0400

    fixed logger

--- frontend/app/lib/logger.ts
+++ frontend/app/lib/logger.ts
@@ -3,7 +3,7 @@ import { consola } from 'consola/browser';
 
 export function getLogger(loggerName: string): ConsolaInstance {
   const log = consola.withTag(loggerName);
-  if (process.env.NODE_ENV === 'dev' || import.meta.env.DEV) log.level = 5;
+  if (process.env.NODE_ENV === 'dev') log.level = 5;
   else log.level = 1;
   return log;
 }
